94b4dcab3b8b72cc203587882992e5dac4e75119
tieudongta <nhiem.nguyentri@gmail.com>
2021-02-28 17:39:34 -0800

    Unit 1 lesson A
    - Add 1 farmer, 1 food item and 3 animals to scene.
    - Scale x3 food
    - Add PlayerController Script to Player
    - Create public variable to get player horizontal input
    - Translate player position based on user input, time and speed.
    - use if, else iff to keep player in bounds.
    - create variable to store player's boundaries.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private float boundary = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        if (transform.position.x > boundary)
+        {
+            transform.position = new Vector3(boundary, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -boundary)
+        {
+            transform.position = new Vector3(-boundary, transform.position.y, transform.position.z);
+        }
+    }
+}
