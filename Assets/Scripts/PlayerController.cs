266023286b984a06231b2ef1913144cc20646caa
tieudongta <nhiem.nguyentri@gmail.com>
2021-02-28 18:42:22 -0800

    Unit 1 Lesson B
    - Create MoveForward Scripts
    - Create Projectile Prefab
    - Use GetKeyDown method in PlayerController
    - Initialize projectile from player position
    - Rotate Animals
    - Destroy gameObject using scene boundary

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
     private float speed = 10.0f;
     private float boundary = 20.0f;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        //Move player horizontally
+        horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        //Set boundary for player
         if (transform.position.x > boundary)
         {
             transform.position = new Vector3(boundary, transform.position.y, transform.position.z);
@@ -24,6 +28,11 @@ public class PlayerController : MonoBehaviour
         else if (transform.position.x < -boundary)
         {
             transform.position = new Vector3(-boundary, transform.position.y, transform.position.z);
+        }
+        //Fire projectiles
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
